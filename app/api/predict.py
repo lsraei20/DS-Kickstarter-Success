71d2a8f7a727e7cca9df254dea3f81c4a795807d
Ivan Campos <39417756+IvanCampos11@users.noreply.github.com>
2020-08-26 21:38:42 -0400

    Update predict.py

--- app/api/predict.py
+++ app/api/predict.py
@@ -1,6 +1,4 @@
 import logging
-import random
-
 from fastapi import APIRouter
 import pandas as pd
 from pydantic import BaseModel, Field, validator
@@ -35,7 +33,7 @@ class Success(BaseModel):
     def title_must_be_str(cls, value):
         assert value.isdigit(
         ) == False, f'{value} == title, title value must be a string'
-        return value
+        return value,
 
     @validator('blurb')
     def blurb_must_be_str(cls, value):
@@ -72,7 +70,7 @@ async def predict(success: Success):
     """
     Make a prediction of kickstarter success or fail
 
-    ### Request Body
+    # Request Body
      - 'title': 'string (title of campaign)',
      - 'blurb': 'string (Description of campaign)',
      - 'goal': 'int (monetary goal)',
@@ -80,16 +78,48 @@ async def predict(success: Success):
      - 'deadline': 'string (date in dd/mm/yyyy format)',
      - 'category': 'string (category of campaign)'
 
-    ### Response
+    # Response
     - `campaign id`: unique campaign identifier
     - `prediction`: boolean, pass or fail,
     representing the predicted class's probability
 
     """
 
-    campaign_id = 23548
-    result = 'pass'
+    response = {
+        "title": "Water bike",
+        "blurb": "A bike that floats",
+        "goal": 5000,
+        "launch_date": "08/06/2020",
+        "deadline": "10/20/2020",
+        "category": "sports"
+    }
+
+    if len(response.get('title')) > 15:
+        Title_feedback = 'too long'
+    elif len(response.get('title')) < 5:
+        Title_feedback = 'too short'
+    else:
+        Title_feedback = 'good'
+
+    if len(response.get('blurb')) > 300:
+        description_feedback = 'too long'
+    elif len(response.get('blurb')) < 50:
+        description_feedback = 'too short'
+    else:
+        description_feedback = 'good'
+
+    prediction = 0
+    probability_of_success = 75
+    monetary_feedback = 'too long'
+    campaign_time_feedback = 'to long'
+    month_feedback = 'wait a month'
+
     return {
-        'campaign id': campaign_id,
-        'prediction': result
+        'prediction': prediction,
+        'probability_of_success': probability_of_success,
+        'monetary_feedback': monetary_feedback,
+        'Title_feedback': Title_feedback,
+        'description_feedback': description_feedback,
+        'campaign_time_feedback': campaign_time_feedback,
+        'month_feedback': month_feedback
     }
